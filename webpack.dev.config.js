a362398d26478a0d2d8addf81bd8abea9fa96ebc
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-02-06 13:56:36 +0800

    Update webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,14 +1,20 @@
 const webpack = require('webpack');
 
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 const merge = require('webpack-merge');
+
 
 const webpackBaseConfig = require('./webpack.base.config.js');
 
+
 const fs = require('fs');
+
 
 fs.open('./src/config/env.js', 'w', function (err, fd) {
     const buf = 'export default "development";';
