db14522d28a1b0d1a195a35d516873d92fe6fd9f
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-05-12 11:26:39 +0800

    Update webpack.base.config.js

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 module.exports = {
     entry: {
         main: './src/main',
